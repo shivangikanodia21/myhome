7313b406d9b0fda18271e321189715e0870e1d24
HP <HP@HP-PC>
2016-06-22 21:24:29 +0530

    initial check in

---
+++ pomgit/src/pomgit/prin.java
@@ -0,0 +1,13 @@
+package pomgit;
+
+public class prin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hello");
+	}
+
+}
