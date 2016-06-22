449955dd42090ecef8f9c3d0db8d8a27d4561d42
HP <HP@HP-PC>
2016-06-22 21:26:03 +0530

    add print1

--- pomgit/src/pomgit/prin.java
+++ pomgit/src/pomgit/prin.java
@@ -8,6 +8,7 @@ public class prin {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("hello");
+        System.out.println("hell1");
 	}
 
 }
